7f77a9bd1880dc371b1a359998a6e84e8629b66e
victorboleac <victor.boleac@colorado.edu>
2024-03-23 01:53:32 +0200

    comments added

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import json
 import requests
-
+# comments added
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "https://uk.indeed.com"}})
 
